1a2720d9b620df24bb25e658215cdf08f22b348a
fernandiiin27 <126727357+fernandiiin27@users.noreply.github.com>
2023-03-08 21:07:21 -0300

    Add files via upload

---
+++ 8-ifelse.php
@@ -0,0 +1,15 @@
+<?php
+$nome = "fernando";
+if($nome == "fernando"){
+	echo "fernando lost";
+} elseif($nome == "fernando e solimoes"){
+	echo "fernando juntos e naw";
+}elseif($nome =="pero"){
+	echo "o fernando esta estudando duplicado";
+}else{
+	echo "ninguem estuda nesse lugar";
+}
+
+
+
+?>
